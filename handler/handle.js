ac5ba3d9b7d40337b8f07f7a914dd50da5dea868
jatin8130 <140238387+jatin8130@users.noreply.github.com>
2026-01-01 11:56:58 +0530

    Refactor user route handlers in handle.js

--- handler/handle.js
+++ handler/handle.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router();
 
-router.route('/users').get(require('../controller/controller').userAdmin).post(require('../controller/controller').emailPost);
+router.route('/users').get(require('../controller/controller').userAdmin).post(require('../controller/controller').Post);
 router.route('/users/:id').post(require('../controller/controller').emailOne).put(require('../controller/controller').Put).delete(require('../controller/controller').Delete);
 
 router.route('/users/email/:id').get(require('../controller/controller').getOne).patch(require('../controller/controller').Patch);
@@ -19,5 +19,6 @@ router.route('/categories/:id').get(require('../controller/controller').getOne).
 
 router.route('/carts/:id').get(require('../controller/controller').getUser).delete(require('../controller/controller').Delete);
 router.route('/carts').post(require('../controller/controller').Post);
+
 
 module.exports = router;
